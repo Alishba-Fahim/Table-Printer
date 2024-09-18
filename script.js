4d0acedf151fdb585306162c5ef8020a4df44ea8
Alishba-Fahim <alishbafahim112@gmail.com>
2024-09-18 22:29:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function printTable() {
+  let tableNum = document.getElementById('NumToPrint').value;
+  let minLength = document.getElementById('minLength').value;
+  let maxLength = document.getElementById('maxLength').value;
+
+  for (let i = minLength; i <= maxLength; i++) {
+    document.getElementById('displayTable').innerHTML += `${tableNum} x ${i} = ${tableNum * i} </br> `;
+  }
+  console.log('work');
+}
+
+function refresh() {
+  document.getElementById('NumToPrint').value = "";
+  document.getElementById('minLength').value = "";
+  document.getElementById('maxLength').value = "";
+  document.getElementById('displayTable').innerHTML = "";
+}
